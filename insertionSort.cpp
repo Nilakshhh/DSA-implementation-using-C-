2a76de8f6e82f1a2a04341b3b4478d7bd0e0fe58
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2023-05-13 20:04:45 +0530

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,1 +1,28 @@
-//to be added
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int a[], int n){
+    for(int i=1;i<n;i++){
+        int j=i;
+        while(a[j]<a[j-1] && j>0){
+            swap(a[j], a[j-1]);
+            j--;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    insertionSort(a, n);
+    for(int i=0;i<n;i++){
+        cout << a[i] << " ";
+    }
+    return 0;
+}
